f53db990cc2749c8549bcf02c21ea2916217ebb8
Luca <lucaan@ethz.ch>
2024-01-12 10:57:17 +0100

    f

--- src/app.rs
+++ src/app.rs
@@ -3,77 +3,20 @@ use chrono::NaiveDate;
 use egui_extras::{Column, DatePickerButton, TableBuilder};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::Pool;
-#[derive(serde::Deserialize, serde::Serialize)]
 pub struct BookFromTable {
     title: String,
     authors: Vec<String>,
     num_pages: u64,
-    #[serde(with = "naive_date_format")]
     acquisition_date: NaiveDate,
-    #[serde(with = "optional_naive_date_format")]
     start_date: Option<NaiveDate>,
-    #[serde(with = "optional_naive_date_format")]
     end_date: Option<NaiveDate>,
     price_ebook: f32,
     price_paperback: f32,
 }
 
-mod naive_date_format {
-    use chrono::NaiveDate;
-    use serde::{self, Deserialize};
-
-    pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let s = date.format("%Y-%m-%d").to_string();
-        serializer.serialize_str(&s)
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        NaiveDate::parse_from_str(&s, "%Y-%m-%d").map_err(serde::de::Error::custom)
-    }
-}
-
-mod optional_naive_date_format {
-    use chrono::NaiveDate;
-    use serde::{self, Deserialize};
-
-    pub fn serialize<S>(date: &Option<NaiveDate>, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        match date {
-            Some(d) => {
-                let s = d.format("%Y-%m-%d").to_string();
-                serializer.serialize_str(&s)
-            }
-            None => serializer.serialize_none(),
-        }
-    }
-
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<NaiveDate>, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s: Option<String> = Option::deserialize(deserializer)?;
-        match s {
-            Some(date_str) => NaiveDate::parse_from_str(&date_str, "%Y-%m-%d")
-                .map(Some)
-                .map_err(serde::de::Error::custom),
-            None => Ok(None),
-        }
-    }
-}
-
 pub struct TemplateApp {
     // Example stuff:
     query_str: String,
-    value: f32,
     db_connection: Pool<sqlx::Postgres>,
     table: Option<Vec<BookFromTable>>,
 }
@@ -97,7 +40,6 @@ impl Default for TemplateApp {
         Self {
             // Example stuff:
             query_str: "".to_owned(),
-            value: 2.3,
             db_connection: pool,
             table: Option::None,
         }
@@ -109,9 +51,6 @@ impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
 
         Default::default()
     }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,192 @@
-// use std::io;
-// mod db;
-// mod requests;
-
-// #[tokio::main]
-// async fn main() -> Result<(), sqlx::Error> {
-//     let mut query = String::new();
-//     println!("Enter book to search");
-//     io::stdin()
-//         .read_line(&mut query)
-//         .expect("Failed to read line");
-//     let value = requests::search(query)
-//         .await
-//         .expect("Query returned an error");
-//     let books = requests::json_to_books(value);
-
-//     println!("Choose which book to add:");
-//     let mut choice = String::new();
-//     for (i, book) in books.iter().enumerate() {
-//         println!("{i}: {book}");
-//     }
-//     io::stdin()
-//         .read_line(&mut choice)
-//         .expect("Failed to read line");
-//     let index = choice.trim().parse::<usize>().expect("Invalid input!");
-
-//     let pool = db::connect().await?;
-
-//     db::insert_book(&books[index], &pool).await?;
-//     db::all_authors(&pool).await?;
-//     db::all_books(&pool).await?;
-//     db::all_references(&pool).await?;
-
-//     Ok(())
-// }
-
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use book_app::TemplateApp;
+use chrono::NaiveDate;
+use egui_extras::{Column, DatePickerButton, TableBuilder};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::Pool;
+
+pub struct BookFromTable {
+    title: String,
+    authors: Vec<String>,
+    num_pages: u64,
+    acquisition_date: NaiveDate,
+    start_date: Option<NaiveDate>,
+    end_date: Option<NaiveDate>,
+    price_ebook: f32,
+    price_paperback: f32,
+}
+
+pub struct TemplateApp {
+    // Example stuff:
+    query_str: String,
+    db_connection: Pool<sqlx::Postgres>,
+    table: Option<Vec<BookFromTable>>,
+}
+
+pub async fn all_read_books(pool: &Pool<sqlx::Postgres>) -> Result<Vec<Record>, sqlx::Error> {
+    let result = sqlx::query!("SELECT 
+    b.title AS title,
+    string_agg(a.name, ', ') AS authors,
+    b.num_pages AS num_pages,
+    b.acquisition_date AS acquisition_date,
+    b.start_date AS start_date,
+    b.end_date AS end_date,
+    b.price_ebook AS price_ebook
+FROM 
+    Books b
+JOIN 
+    BookAuthors ba ON b.book_id = ba.book_id
+JOIN 
+    Authors a ON ba.author_id = a.author_id
+GROUP BY 
+    b.book_id;").fetch_all(pool).await?;
+    result
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let database_url = "postgres://postgres:mysecretpassword@localhost/postgres";
+        let pool = rt
+            .block_on(async {
+                PgPoolOptions::new()
+                    .max_connections(5)
+                    .connect(database_url)
+                    .await
+            })
+            .unwrap();
+
+        let mut def = Self {
+            // Example stuff:
+            query_str: "".to_owned(),
+            db_connection: pool,
+            table: Option::None,
+        };
+        def.table = 
+    }
+}
+
+impl TemplateApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        Default::default()
+    }
+}
+
+impl eframe::App for TemplateApp {
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            _frame.close();
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_dark_light_mode_buttons(ui);
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("eframe template");
+
+            ui.horizontal(|ui| {
+                ui.label("Enter Book:");
+                ui.text_edit_singleline(&mut self.query_str);
+                if ui.button("Search").clicked() {
+                    println!("Searching for {}...", self.query_str);
+                }
+            });
+
+            ui.heading("List of Read Books");
+
+            ui.vertical(|ui| {
+                let text_height = egui::TextStyle::Body.resolve(ui.style()).size;
+                let table = TableBuilder::new(ui)
+                    .striped(true)
+                    .resizable(true)
+                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                    .column(Column::auto())
+                    .column(Column::initial(100.0).range(40.0..=300.0))
+                    .column(Column::initial(100.0).at_least(40.0).clip(true))
+                    .column(Column::initial(100.0))
+                    .column(Column::initial(100.0))
+                    .column(Column::initial(100.0))
+                    .column(Column::initial(100.0))
+                    .column(Column::initial(100.0))
+                    .min_scrolled_height(0.0);
+
+                table
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.strong("Title");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Authors");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Number of Pages");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Acquisition Date");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Start Date");
+                        });
+                        header.col(|ui| {
+                            ui.strong("End Date");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Price eBook");
+                        });
+                        header.col(|ui| {
+                            ui.strong("Price Paperback");
+                        });
+                    })
+                    .body(|body| {
+                        body.rows(text_height, 10, |row_index, mut row| {
+                            row.col(|ui| {
+                                ui.label(row_index.to_string());
+                            });
+                            row.col(|ui| {
+                                ui.label("Column 1");
+                            });
+                            // row.col(|ui| {
+                            //     if row_index % 2 == 0 {
+                            //         let mut date = NaiveDate::from_ymd_opt(1070, 1, 1).unwrap();
+                            //         ui.add(DatePickerButton::new(&mut date));
+                            //     }
+                            //     ui.label("Column 2");
+                            // });
+                            row.col(|ui| {
+                                ui.add(
+                                    egui::Label::new("Thousands of rows of even height")
+                                        .wrap(false),
+                                );
+                            });
+                        })
+                    });
+            });
+        });
+    }
+}
 
 fn main() -> eframe::Result<()> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
