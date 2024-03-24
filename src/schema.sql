026090ee7e90b2bac0e03cff6099d3c22d31ffc6
Luca <lucaan@ethz.ch>
2024-03-24 22:02:37 +0100

    split "books" table into "owned_books" and "read_books" in SQL database

--- src/schema.sql
+++ src/schema.sql
@@ -1,25 +1,30 @@
-DROP TABLE IF EXISTS Books, Authors, BookAuthors;
+DROP TABLE IF EXISTS owned_books, read_books, authors, book_authors;
 
-CREATE TABLE Books (
+CREATE TABLE owned_books (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(63) NOT NULL,
     num_pages INTEGER NOT NULL,
     acquisition_date DATE NOT NULL,
-    start_date DATE,
-    end_date DATE,
     price_ebook DECIMAL(5,2),
     price_paperback DECIMAL(5,2),
-    CONSTRAINT no_negative_prices CHECK (price_ebook >= 0 AND price_paperback >= 0),
-    CONSTRAINT check_order CHECK (acquisition_date <= start_date AND start_date <= end_date)
+    CONSTRAINT no_negative_prices CHECK (price_ebook >= 0 AND price_paperback >= 0)
 );
 
-CREATE TABLE Authors (
+CREATE TABLE read_books (
+    book_id INTEGER REFERENCES owned_books(book_id) ON DELETE CASCADE,
+    PRIMARY KEY (book_id),
+    start_date DATE,
+    end_date DATE,
+    CONSTRAINT check_order CHECK (start_date <= end_date)
+);
+
+CREATE TABLE authors (
     author_id SERIAL PRIMARY KEY,
     name VARCHAR(63) UNIQUE NOT NULL
 );
 
-CREATE TABLE BookAuthors (
-    book_id INTEGER REFERENCES Books(book_id) ON DELETE CASCADE,
-    author_id INTEGER REFERENCES Authors(author_id) ON DELETE CASCADE,
+CREATE TABLE book_authors (
+    book_id INTEGER REFERENCES owned_books(book_id) ON DELETE CASCADE,
+    author_id INTEGER REFERENCES authors(author_id) ON DELETE CASCADE,
     PRIMARY KEY (book_id, author_id)
 );
