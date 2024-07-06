b22ef2f71419d2c864a68286d664e0ef0a4f715c
Luca <lucaan@ethz.ch>
2024-07-06 13:26:35 +0200

    updated subomdule

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,19 @@
-use egui::Ui;
+use egui::{Spinner, Ui};
 use egui_extras::{Column, TableBuilder};
-use goodreads_api::goodreads_api;
 use ::goodreads_api::goodreads_api::GoodreadsBook;
+use tokio::runtime::Runtime;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
     label: String,
-    books: Vec<GoodreadsBook>
+    #[serde(skip)]
+    books: Vec<GoodreadsBook>,
+    #[serde(skip)]
+    search_button_clicked: bool,
+    #[serde(skip)]
+    rt: Runtime,
 }
 
 impl Default for TemplateApp {
@@ -16,7 +21,9 @@ impl Default for TemplateApp {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
-            books: vec![]
+            books: vec![],
+            search_button_clicked: false,
+            rt: Runtime::new().expect("Error creating runtime")
         }
     }
 }
@@ -71,44 +78,71 @@ impl eframe::App for TemplateApp {
             ui.horizontal(|ui| { 
                 ui.label("Enter Query: ");
                 ui.text_edit_singleline(&mut self.label);
-                if ui.button("Enter").clicked() {
-                    // Do something when the button is clicked
-                    self.books = goodreads_api::GoodreadsBook::search(self.label.as_str());
+                self.search_button_clicked = ui.button("Enter").clicked();
+                
+            });
+
+            ui.vertical(|ui| {
+                
+                ui.add(Spinner::new());
+                if self.search_button_clicked {
+                    self.books = self.rt.block_on(async {
+                        GoodreadsBook::search(self.label.as_str()).await});
+                    
                     for book in &self.books {
                         println!("{book}");
                     }
                 }
-                
+                if !self.books.is_empty() {
+                    table_ui(ui, self.books.clone());
+                }
             });
-            ui.vertical(|ui| {
-                table_ui(ui, self.books.clone()); 
-            });
-            
-            
     });
 }
 }
 
 fn table_ui(ui: &mut Ui, books: Vec<GoodreadsBook>) {
     TableBuilder::new(ui)
-                    .column(Column::auto().resizable(true))
-                    .column(Column::auto().resizable(true))
-                    .header(20.0, |mut header| {
-                        header.col(|ui| {
-                            ui.heading("First column");
-                        });
-                        header.col(|ui| {
-                            ui.heading("Second column");
-                        });
-                    })
-                    .body(|mut body| {
-                        body.row(20.0, |mut row| {
-                            row.col(|ui| {
-                                ui.label("Hello");
-                            });
-                            row.col(|ui| {
-                                ui.button("world!");
-                            });
-                        });
+            .columns(Column::auto().resizable(true), 5)
+            .header(20.0, |mut header| {
+                header.col(|ui| {
+                    ui.strong("Title");
+                });
+                header.col(|ui| {
+                    ui.strong("Author(s)");
+                });
+                header.col(|ui| {
+                    ui.strong("No. Pages");
+                });
+                header.col(|ui| {
+                    ui.strong("Series");
+                });
+                header.col(|ui| {
+                    ui.strong("Volume");
+                });
+            })
+            .body(|mut body| {
+
+                for book in books {
+                    body.row(20.0, |mut row| {
+                    row.col(|ui| {
+                        ui.label(book.title());
                     });
+                    row.col(|ui| {
+                        ui.label(book.authors().join(","));
+                    });
+                    row.col(|ui| {
+                        ui.label(book.pages().to_string());
+                    });
+                    row.col(|ui| {
+                        ui.label(book.series().unwrap_or("None".to_string()));
+                    });
+                    row.col(|ui| {
+                        ui.label({match book.index() {
+                            None => "None".to_string(),
+                            Some(f) => f.to_string()
+                        }});
+                    });
+                });}
+            });
 }
