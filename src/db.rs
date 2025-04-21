92d510ccdcf12785d1c2d07ba6841561e8d20593
Luca <luca.antonelli@gmx.ch>
2025-04-21 22:41:37 +0200

    started reworking database calls to use sqlx::query_as!, including necessary changes to structs

--- src/db.rs
+++ src/db.rs
@@ -6,10 +6,8 @@ use goodreads_api::goodreads_api;
 use log::info;
 
 pub struct OwnedBook {
-    book_id: u32,
     title: String,
-    authors: Vec<Author>,
-    num_pages: u64,
+    num_pages: i32,
     acquisition_date: Option<NaiveDate>,
     url: String,
     cover_url: String
@@ -22,32 +20,17 @@ pub struct ReadBook {
 
 }
 
-pub struct Author {
-    first_name: String,
-    last_name: String
-}
-
-impl From<String> for Author {
-    fn from(value: String) -> Self {
-        let mut parts = value.split_whitespace();
-        let first_name = parts.next().expect("No first name found!").to_owned();
-        let last_name = parts.next().unwrap_or("").to_owned();
-
-        Self{ first_name, last_name}
-    }
-}
 
 
 
-impl From<goodreads_api::GoodreadsBook> for Book {
+impl From<goodreads_api::GoodreadsBook> for OwnedBook {
     fn from(value: goodreads_api::GoodreadsBook) -> Self {
         Self {
             title: value.title(),
-            authors: value.authors().iter().map(|x| Author::from(x.to_owned())).collect::<Vec<Author>>(),
-            num_pages: value.pages(),
+            num_pages: value.pages() as i32,
             acquisition_date: None,
-            start_date: None,
-            end_date: None,
+            url: value.url(),
+            cover_url: value.cover_image().unwrap_or("".to_string())
         }
     }
 }
@@ -67,7 +50,7 @@ impl DataBaseConnection {
 
     pub async fn insert_owned_book(&self, input_book: GoodreadsBook) -> Result<(), sqlx::Error> {
         info!("Inserting new book into database");
-        let book = Book::from(input_book);
+        let book = OwnedBook::from(input_book);
         sqlx::query!(
             "INSERT INTO owned_books (title, num_pages, acquisition_date) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING",
             book.title,
@@ -78,8 +61,8 @@ impl DataBaseConnection {
     
         for author in &book.authors {
             sqlx::query!(
-                "INSERT INTO authors (first_name, last_name) VALUES ($1, $2) ON CONFLICT (first_name, last_name) DO NOTHING",
-                author.first_name,
+                "INSERT INTO authors (given_names, last_name) VALUES ($1, $2) ON CONFLICT (given_names, last_name) DO NOTHING",
+                author.given_names,
                 author.last_name
             )
             .execute(&self.0)
@@ -87,8 +70,8 @@ impl DataBaseConnection {
             sqlx::query!(
                 "INSERT INTO book_authors (book_id, author_id) VALUES (
                     (SELECT book_id FROM owned_books WHERE title = $1), 
-                    (SELECT author_id FROM authors WHERE first_name = $2 AND last_name = $3)) ON CONFLICT DO NOTHING", 
-                book.title, author.first_name, author.last_name)
+                    (SELECT author_id FROM authors WHERE given_names = $2 AND last_name = $3)) ON CONFLICT DO NOTHING", 
+                book.title, author.given_names, author.last_name)
                 .execute(&self.0).
                 await?;
         }
@@ -113,15 +96,15 @@ impl DataBaseConnection {
         Ok(())
     }
 
-    pub async fn all_authors(&self) -> Result<(), sqlx::Error> {
-        let query_result = sqlx::query!("SELECT * FROM authors")
+    pub async fn all_authors(&self) -> Result<Vec<goodreads_api::Author>, sqlx::Error> {
+        let query_result = sqlx::query_as!(goodreads_api::Author, "SELECT given_names, last_name FROM authors")
             .fetch_all(&self.0)
             .await?;
-        for row in query_result {
+        for row in &query_result {
             println!("{:?}", row);
         }
     
-        Ok(())
+        Ok(query_result)
     }
 
     pub async fn alter_start_date(&self, book_id: i32, start_date: NaiveDate) -> Result<(), sqlx::Error> {
@@ -140,7 +123,11 @@ impl DataBaseConnection {
         Ok(())
     }
 
-    pub async fn get_all_owned_books(&self) -> Result<Vec<Book>, sqlx::Error> {
-        sqlx::query_as!(Book, "SELECT * FROM owned_books").fetch_all(&self.0).await
+    pub async fn get_all_owned_books(&self) -> Result<Vec<OwnedBook>, sqlx::Error> {
+        let result = sqlx::query_as!(OwnedBook, "SELECT title, num_pages, acquisition_date, url, cover_url FROM owned_books")
+            .fetch_all(&self.0)
+            .await?;
+
+        Ok(result)
     }
 }
